b0de45dfe1824dbb7e9fcb4467f740f8fdbc8786
Folgue02 <davidfolru@gmail.com>
2021-06-24 14:19:41 +0200

    New login and register templates# Please enter the commit message for your changes. Lines starting

--- templates/template.py
+++ templates/template.py
@@ -15,7 +15,7 @@ def server_message(message:str) -> str:
     return dumps({"type": "server", "msg": message})
 
 
-def direct_message(author_id:int, author_name:str, message:str) -> str:
+def direct_message(author_id: int, author_name: str, message: str) -> str:
     return dumps({"type": "direct_message", "author_name": author_name, "author_id": author_id, "message": message})
 
 
@@ -26,6 +26,16 @@ def command_message(command: str, parameters: list):
 def command_result(code: int) -> str:
     # Format of message that contains the code about a command used. (Used by the server and received by the client)
     return dumps({"type":"result", "code":code})
+
+
+def register(name: str, passwd: str) -> str:
+    # Message sent from the client to the server in order to create account
+    return dumps({"type": "register", "name": name, "passwd": passwd})
+
+
+def login(name: str, passwd: str) -> str:
+    # Message sent from the client to the server in order to login into an existing account
+    return dumps({"type": "login", "name": name, "passwd": passwd})
 
 # TODO Keep adding new types of messages
 

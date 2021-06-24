378d5843ccdb735149f354e2168053bfe884c52c
Folgue02 <davidfolru@gmail.com>
2021-06-24 14:19:06 +0200

    New codes added

--- share/codes.py
+++ share/codes.py
@@ -2,9 +2,12 @@
 # Codes are returned by the server to give a response to the client about the execution of 
 # a command.
 CODES = {
+    0: "Everything went well",
     1: "Internal error",
     2: "Command not found.",
     3: "Wrong parameter usage.",
     4: "Not allowed to use command.",
-    5: "Invalid usage."
+    5: "Invalid usage.",
+    6: "Wrong credentials",
+    7: "Couldn't register account."
 }
